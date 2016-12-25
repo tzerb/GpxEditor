c9048c8ba46f3643aed08a0b7982876f63cb4ed9
Tim Zirbel <tzerb@hotmail.com>
2016-12-25 17:23:33 -0600

    Read gpx and identify number of waypoints

--- src/components/GetGpxFile.tsx
+++ src/components/GetGpxFile.tsx
@@ -18,12 +18,23 @@ export class GetGpxFile extends React.Component<GetGpxFileProps, GetGpxFileState
         this.fileChanged = this.fileChanged.bind(this);
     }
 
-    fileChanged(e : any)
+    fileChanged(ev : React.FormEvent<HTMLInputElement>)
     {
         // 
-        alert('fileChanged' + e.target.files[0].size);
+        //alert('fileChanged' + e.target.files[0].size);
+        var reader = new FileReader();
+        
+        reader.readAsText((ev as any).target.files[0]);
+        reader.onloadend = function(){
+
+            let parser = new DOMParser()
+            let xmlDom = parser.parseFromString(reader.result, "text/xml");    
+            let waypoints = xmlDom.documentElement.getElementsByTagName("wpt")        
+            alert(waypoints.length + ' points');
+            var xmlData = reader.result;
+        };
     }
-    
+    //React.EventHandler<React.FormEvent<HTMLInputElement>>
     render() {
         return (<div>
                     <input type="file" id="the-gpx-file-field" onChange={this.fileChanged}/>
